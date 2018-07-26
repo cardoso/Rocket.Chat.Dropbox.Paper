0f3268e7dd710e4693c7a7e1ef2a6a68a27f732d
cardoso <matheus@cardo.so>
2018-07-26 16:04:28 -0300

    🔨 fix: missing await

--- DropboxPaperApp.ts
+++ DropboxPaperApp.ts
@@ -1,9 +1,8 @@
 import {
     IConfigurationExtend,
-    IConfigurationModify,
     IEnvironmentRead,
     IHttp,
-    ILogger,
+    IPersistence,
     IRead,
 } from '@rocket.chat/apps-ts-definition/accessors';
 
@@ -13,11 +12,7 @@ import { IAppInfo } from '@rocket.chat/apps-ts-definition/metadata';
 import { DropboxCreatePaperCommand } from './commands/DropBoxCreatePaperCommand';
 
 export class DropboxPaperApp extends App {
-    constructor(info: IAppInfo, logger: ILogger) {
-        super(info, logger);
-    }
-
     protected async extendConfiguration(configuration: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void> {
-        configuration.slashCommands.provideSlashCommand(new DropboxCreatePaperCommand(this));
+        await configuration.slashCommands.provideSlashCommand(new DropboxCreatePaperCommand(this));
     }
 }
